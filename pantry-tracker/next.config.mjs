f73a7ffd7d6ebaa131ef4d2eee1537a4f07356d8
Ismailn16 <ismailnoor.789@gmail.com>
2024-09-23 22:47:29 +0100

    second attempt

--- pantry-tracker/next.config.mjs
+++ pantry-tracker/next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-        appDir: true, // This ensures the use of the new `app/` directory
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
